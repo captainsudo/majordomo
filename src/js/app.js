1846b73ba3528983285e7dde7dbbb3c7cebc93d0
Josh Porter <portjsh@gmail.com>
2018-10-25 14:40:08 -0600

    Added slick slider functionality

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,13 @@ angular.module('main',[]);
  angular.module('main').controller('mainController',function($scope){
     // Controller body
     $scope.welcome = 'This is just the beginning.';
+    $('.wizard').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        centerMode: true,
+        variableWidth: true
+    });
 });
 
-$('.wizard').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
